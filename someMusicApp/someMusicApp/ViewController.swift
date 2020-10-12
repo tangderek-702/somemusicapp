dd3c60e34586ecc7e3e1123811ecc59796d277b7
Derek Tang <tangderek702@berkeley.edu>
2020-10-12 16:34:10 -0700

    rest in pieces laptop battery dead

--- someMusicApp/someMusicApp/ViewController.swift
+++ someMusicApp/someMusicApp/ViewController.swift
@@ -13,7 +13,11 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func rejectButton(_ sender: Any) {
+        
+    }
+    @IBAction func acceptButton(_ sender: Any) {
+        
+    }
 }
 
