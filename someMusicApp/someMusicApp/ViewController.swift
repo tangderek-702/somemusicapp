2c379ce107dc4311cb312f45ba5969fa13ff3333
Derek Tang <tangderek702@berkeley.edu>
2020-10-12 16:14:47 -0700

    first commit

---
+++ someMusicApp/someMusicApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  someMusicApp
+//
+//  Created by Derek Tang on 10/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
