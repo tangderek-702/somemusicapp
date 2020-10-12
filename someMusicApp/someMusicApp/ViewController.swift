edfb213bca3f5d4629046ffa2ff5dc8566e860a0
Jennifer Yang <jennifer.yang818@gmail.com>
2020-10-12 16:58:00 -0700

    new viewcontroller added

--- someMusicApp/someMusicApp/ViewController.swift
+++ someMusicApp/someMusicApp/ViewController.swift
@@ -14,10 +14,11 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     @IBAction func rejectButton(_ sender: Any) {
+        self.performSegue(withIdentifier: "secondView", sender: self)
         
     }
     @IBAction func acceptButton(_ sender: Any) {
-        
+        self.performSegue(withIdentifier: "secondView", sender: self)
     }
 }
 
