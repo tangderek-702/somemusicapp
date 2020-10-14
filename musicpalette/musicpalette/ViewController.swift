f28c86e3b9c927c1ad0c27621edb3b7a482ccfbd
Jennifer Yang <jennifer.yang818@gmail.com>
2020-10-13 18:43:26 -0700

    testing hacky way of making a team

---
+++ musicpalette/musicpalette/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  musicpalette
+//
+//  Created by User on 10/13/20.
+//  Copyright © 2020 musicpalette. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
