1339e6ff9ff1e1c1a6669c109cafa41f9fd993c3
Jennifer Yang <jennifer.yang818@gmail.com>
2020-10-13 19:37:00 -0700

    revolving through four artists, super basic lol

--- musicpalette/musicpalette/ViewController.swift
+++ musicpalette/musicpalette/ViewController.swift
@@ -9,12 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let artists = ["joji", "rich brian", "drake", "niki"]
+    var counter = 0
+    var yay: [String] = []
 
+    @IBOutlet weak var artistLabel: UILabel!
+    
+    @IBOutlet weak var yayButton: UIButton!
+    @IBOutlet weak var nayButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    
+    @IBAction func yayButtonClicked(_ sender: Any) {
+        if counter == artists.count {
+            counter = 0
+        }
+        artistLabel.text = artists[counter]
+        counter = counter + 1
+    }
+    @IBAction func nayButtonClicked(_ sender: Any) {
+        if counter == artists.count {
+            counter = 0
+        }
+        artistLabel.text = artists[counter]
+        counter = counter + 1
+    }
+    
 }
 
