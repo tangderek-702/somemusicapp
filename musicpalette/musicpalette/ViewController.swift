5ab95c3eaf1655fa05c49c6ed29eddda384acd78
Jennifer Yang <jennifer.yang818@gmail.com>
2020-10-18 22:27:31 -0700

    added dictionary of artists to songs, generates random song for each artist swiped on. still look ugly as heck

--- musicpalette/musicpalette/ViewController.swift
+++ musicpalette/musicpalette/ViewController.swift
@@ -10,15 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let artists = ["joji", "rich brian", "drake", "niki"]
+    let artists = ["Joji", "Rich Brian", "Drake", "Niki"]
+    let songs = ["Joji": ["Slow Dancing in the Dark", "Daylight", "Tick Tock", "Ew", "Run"],
+        "Rich Brian": ["Love in my Pocket", "100 Degrees", "Midsummer Madness", "Yellow", "Sometimes", "When You Come Home"],
+        "Drake": ["Hotline Bling", "Headlines", "Toosie Slide", "God's Plan", "One Dance", "In My Feelings"],
+        "Niki": ["Indigo", "Nightcrawlers", "lowkey", "I Like U", "See U Never", "La La Lost You"]]
+//    let songs = ["Joji": "Daylight",
+//    "Rich Brian": "100 Degrees",
+//    "Drake": "Hotline Bling",
+//    "Niki": "Indigo"]
     var counter = 0
     var yay: [String] = []
+    var playlist: [String] = []
 
     @IBOutlet weak var artistLabel: UILabel!
     
     @IBOutlet weak var yayButton: UIButton!
     @IBOutlet weak var nayButton: UIButton!
     @IBOutlet weak var artistsSoFar: UILabel!
+    @IBOutlet weak var songsSoFar: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -26,14 +37,28 @@ class ViewController: UIViewController {
     
     @IBAction func yayButtonClicked(_ sender: Any) {
         artistsSoFar.text = "";
+        songsSoFar.text = "";
+        
         if counter == artists.count {
             counter = 0
         }
         yay.append(artistLabel.text!)
+        
+        var artistSongs = songs[artistLabel.text ?? "song"]
+        
+        var addSong = (artistSongs?.randomElement()!)
+        while playlist.contains(addSong ?? "song") {
+            addSong = (artistSongs?.randomElement()!)
+        }
+        playlist.append(addSong ?? "song")
+        
         artistLabel.text = artists[counter]
         counter = counter + 1
         for name in yay {
             artistsSoFar.text?.append(name)
+        }
+        for name in playlist {
+            songsSoFar.text?.append(name)
         }
     }
     @IBAction func nayButtonClicked(_ sender: Any) {
