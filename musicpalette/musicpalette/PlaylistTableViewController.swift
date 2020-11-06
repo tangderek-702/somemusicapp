4fc06f88c8c2282ef0248b92f105407d934903dd
Jennifer Yang <jennifer.yang818@gmail.com>
2020-11-05 17:45:04 -0800

    trying to populate cells

--- musicpalette/musicpalette/PlaylistTableViewController.swift
+++ musicpalette/musicpalette/PlaylistTableViewController.swift
@@ -38,12 +38,23 @@ class PlaylistTableViewController: UITableViewController {
     // Provide a cell object for each row.
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         //TODO: Add Custom TableViewCell to display the cell
-        return UITableViewCell()
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Playlist Table View Cell", for: indexPath) as! PlaylistTableViewCell
+        
+        
+        cell.artistNameLabel.text = swipeModels[indexPath.row].title
+        cell.artistSongsLabel.text = "Songs: \(swipeModels[indexPath.row].subtitle)"
+        //cell.artistImageView.image = UIImage(named: swipeModels[indexPath.row].image)
+        
+        return cell
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        self.tableView.register(UINib(nibName: "PlaylistTableViewCell", bundle: nil), forCellReuseIdentifier: "Playlist Table View Cell")
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
         
